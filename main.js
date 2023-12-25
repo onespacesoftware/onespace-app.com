baaf4644ad467f3b6b8295f4938fe515697bd3ab
Sahith Bodla <bodlasahith@gmail.com>
2023-12-24 22:51:24 -0800

    animated tab

--- main.js
+++ main.js
@@ -644,3 +644,21 @@ function animate() {
   renderer.render(scene, camera);
 }
 animate();
+
+function recursiveAnimateTitle(string) {
+  let firstLetter = string[0];
+  let title = document.querySelector("title");
+  title.innerHTML += firstLetter;
+  if (string.length > 1) {
+    setTimeout(function () {
+      recursiveAnimateTitle(string.substring(1));
+    }, 100);
+  }
+}
+
+function animateTitle(string) {
+  document.querySelector("title").innerHTML = "";
+  recursiveAnimateTitle(string);
+}
+
+animateTitle("OneSpace 💻🚀!!!!!");
